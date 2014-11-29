196e9529d8b7b20400bcbbcecaebcb60932b00fe
Sylwester Zinkiewicz <s.zinkiewicz@gmail.com>
2014-11-29 12:43:31 +0100

    Fixed deprecated functions

--- chapter8/socketio/server.js
+++ chapter8/socketio/server.js
@@ -6,14 +6,16 @@ var express = require('express')
 server.listen(8080);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/app.js', function(req, res) {
-  res.sendfile(__dirname + '/app.js');
+  res.sendFile(__dirname + '/app.js');
 });
 
 io.sockets.on('connection', function (socket) {
+  console.log('Connected on localhost:8888');
+  
   socket.emit('new:msg', 'Welcome to AnonBoard');
 
   socket.on('broadcast:msg', function(data) {
